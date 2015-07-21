9222d6f15f9b62c375203a7df34317927c66d7b4
Kento Kobayashi <kobayashi_kento@nomadinc.co.jp>
2015-07-22 06:03:24 +0900

    fix validation

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,6 +1,6 @@
 class Contribution < ActiveRecord::Base
     validates :contributor, presence: true
-    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :email, allow_blank: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     validates :text, presence: true
 
     paginates_per 10
